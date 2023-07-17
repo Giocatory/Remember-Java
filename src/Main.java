328afa5d0a5435ac35f084821c39b5a745db83f1
Giocatory <nivolyas@gmail.com>
2023-07-17 21:18:52 +0800

    17.07.2023

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Mikhail!!");
+        TypesInJava tj = new TypesInJava();
+
+        tj.showTypes();
+
+        ArrayList<String> types_arr =  tj.get_types_in_java();
+        System.out.println(String.join(", ", types_arr));
     }
 }
---
+++ src/TypesInJava.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class TypesInJava {
+    private final ArrayList<String> arrayList = new ArrayList<String>();
+
+    public TypesInJava() {
+        arrayList.add("byte");
+        arrayList.add("short");
+        arrayList.add("int");
+        arrayList.add("long");
+        arrayList.add("float");
+        arrayList.add("double");
+        arrayList.add("boolean");
+        arrayList.add("char");
+        arrayList.add("String");
+        arrayList.add("Object");
+    }
+
+    public void showTypes() {
+        for (String type : arrayList) {
+            System.out.println(type);
+        }
+    }
+
+    public ArrayList<String> get_types_in_java() {
+        return arrayList;
+    }
+}
