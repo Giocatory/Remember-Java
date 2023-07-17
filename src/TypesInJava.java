21cc31ee7d5aa1b9e1926a13bed4b02b366dd3fa
Giocatory <nivolyas@gmail.com>
2023-07-17 21:22:06 +0800

    17.07.2023

--- src/TypesInJava.java
+++ src/TypesInJava.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class TypesInJava {
+    /**
+     * java types
+     */
     private final ArrayList<String> arrayList = new ArrayList<String>();
 
     public TypesInJava() {
