2fdf54c83f4e3a9077c607a6cf494c86bf36e9f5
Giocatory <nivolyas@gmail.com>
2023-07-18 21:09:26 +0800

    17.07.2023

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Enter number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        var result = switch (n){
+            case 1,3,5,7,9 -> "Even";
+            case 2,4,6,8 -> "Odd";
+            default -> "Invalid";
+        };
+        System.out.println(result);
     }
 }
